a822158fa0fa91dfe67aab305c2d5a6de27c2119
Gustavo Lopes <guhrlopes999@gmail.com>
2025-10-20 17:08:24 -0300

    exercícios POO

---
+++ contabancaria-exercicio/ContaBancaria.cs
@@ -0,0 +1,16 @@
+﻿class ContaBancaria
+{
+    public int numeroIndicador;
+    public string titular;
+    public double saldo;
+    public int senha;
+
+    public void ExibirInfos()
+    {
+        Console.WriteLine($"TITULAR DA CONTA: {titular}");
+        Console.WriteLine($"NUMERO DA CONTA: {numeroIndicador}");
+        Console.WriteLine($"SALDO DA CONTA: {saldo}");
+        Console.WriteLine($"SENHA: {senha}");
+    }
+
+}
---
+++ contabancaria-exercicio/Program.cs
@@ -0,0 +1,7 @@
+﻿ContaBancaria conta1 = new ContaBancaria();
+conta1.numeroIndicador = 1234;
+conta1.titular = "Gustavo";
+conta1.saldo = 240.50;
+conta1.senha = 12345;
+
+conta1.ExibirInfos();
