73d326be976135384992b49cdf6b3809a1e450bc
Gustavo Lopes <guhrlopes999@gmail.com>
2025-10-20 16:58:19 -0300

    Classe música criada

---
+++ screensound-poo/Musica.cs
@@ -0,0 +1,22 @@
+﻿class Musica
+{
+    public string nome;
+    public string artista;
+    public int duracao;
+    public bool disponivel;
+
+
+    public void ExibirFichaTecnica()
+    {
+        Console.WriteLine($"NOME: {nome}");
+        Console.WriteLine($"ARTISTA: {artista}");
+        Console.WriteLine($"DURAÇÃO: {duracao}");
+        if (disponivel)
+        {
+            Console.WriteLine("Disponível no plano.");
+        } else
+        {
+            Console.WriteLine("Adquira o plano plus.");
+        }
+    }
+}
