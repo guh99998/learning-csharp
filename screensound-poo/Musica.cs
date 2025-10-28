7179bf1c13715d6c8cc895ff5133133db604ce3a
Gustavo Lopes <guhrlopes999@gmail.com>
2025-10-28 17:36:47 -0300

    Adicionado get e set na classe

--- screensound-poo/Musica.cs
+++ screensound-poo/Musica.cs
@@ -1,22 +1,29 @@
 ﻿class Musica
 {
-    public string nome;
-    public string artista;
-    public int duracao;
-    public bool disponivel;
-
+    public string Nome {  get; set; }
+    public string Artista {  get; set; }
+    public int Duracao { get; set; }
+    public bool Disponivel { get; set; }
+    public string DescricaoResumida => 
+        $"A música {Nome} pertence à banda {Artista}";
 
     public void ExibirFichaTecnica()
     {
-        Console.WriteLine($"NOME: {nome}");
-        Console.WriteLine($"ARTISTA: {artista}");
-        Console.WriteLine($"DURAÇÃO: {duracao}");
-        if (disponivel)
+        Console.WriteLine($"NOME: {Nome}");
+        Console.WriteLine($"ARTISTA: {Artista}");
+        Console.WriteLine($"DURAÇÃO: {Duracao}");
+        if (Disponivel)
         {
             Console.WriteLine("Disponível no plano.");
         } else
         {
             Console.WriteLine("Adquira o plano plus.");
         }
+    }
+
+    public void ExibirNomeEArtista()
+    {
+        Console.WriteLine($"NOME: {Nome}");
+        Console.WriteLine($"ARTISTA: {Artista}");
     }
 }
